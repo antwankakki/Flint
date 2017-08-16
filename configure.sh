e0ca696d8d7362ff2be685b0e2775f6c6161bb49
Fadi Hanna Al-Kass <Alkass@users.noreply.github.com>
2017-08-16 15:59:31 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,2 +1,4 @@
-apt-get update -yq
-apt-get install -yq valgrind
+apt-get -yq update
+apt-get install -yq --no-install-recommends build-essential valgrind
+apt-get clean
+rm -rf /var/lib/apt/lists/*
