16e76599a4d5a41ad1e36369f572a6c98c586cd6
Fadi Hanna Al-Kass <fadi.eliya@yahoo.com>
2017-08-19 02:21:50 -0500

    documented

--- libs/kernel/defines.h
+++ libs/kernel/defines.h
@@ -1,10 +1,20 @@
 #ifndef __DEFINES__
 #define __DEFINES__
 
+// The compiler has checks enabled to spint out warnings when variables/objects
+// are declared and not used and has switches enabled to treat warnings at errors.
+// If you've declared a variable or an object that you're not yet ready to utilize,
+// pass it to UNUSED() which will let the compiler know that you're aware of this
+// warning and would like to silence it.
 #define UNUSED(x) \
   debugPrint(INFO, __DEBUG__, "Unused parameter (%s) found in %s", #x, __FUNCTION__); \
   (void)x;
 
+// Double freeing leads to segmentation faluts. This can be extremely dangerous
+// especially when the drone is in flight mode as it will very likely run out of
+// control and crash or fly into space.
+// <b>Always</b> avoid using free() directly, and use SAFE_FREE() instead. This
+// rule will be enforced and will lead to <b>pull request rejection</b> when violated.
 #define SAFE_FREE(x) \
   if (x) \
   { \
@@ -14,7 +24,7 @@
   } \
   else \
   { \
-    debugPrint(INFO, __DEBUG__ || __RELEASE__, "%s has been freed already", #x); \
+    debugPrint(INFO, __DEBUG__ || __RELEASE__, "%s is a free pointer", #x); \
   };
 
 #endif
