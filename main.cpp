d4e61ed2e6c5462b994189dfeec1e00e50055973
Fadi Hanna Al-Kass <Alkass@users.noreply.github.com>
2017-08-19 19:37:33 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,14 +12,14 @@ int main() {
         debugPrint(FATAL, TRUE, "Failed to populate application");
         EXIT_POINT_WITH_RETURN(1);
     }
-
-    // test
-    application.handle.showInventory(&application);
-
-    if (!application.handle.start(&application))
+    else
     {
-      debugPrint(FATAL, TRUE, "Unable to start application");
-      EXIT_POINT_WITH_RETURN(1);
+        application.handle.showInventory(&application);
+        if (!application.handle.start(&application))
+        {
+            debugPrint(FATAL, TRUE, "Unable to start application");
+            EXIT_POINT_WITH_RETURN(1);
+        }
     }
     EXIT_POINT_WITH_RETURN(0);
 }
