d2b1ddfd5554d3d90ba7591cb2bdaae1da738e38
Fadi Hanna Al-Kass <Alkass@users.noreply.github.com>
2017-08-24 15:45:40 -0500

    typo fixes

--- libs/kernel/defines.h
+++ libs/kernel/defines.h
@@ -1,8 +1,8 @@
 #ifndef __DEFINES__
 #define __DEFINES__
 
-// The compiler has checks enabled to spint out warnings when variables/objects
-// are declared and not used and has switches enabled to treat warnings at errors.
+// The compiler has checks enabled to spit out warnings when variables/objects are
+// declared and not used and has switches enabled to treat warnings at errors.
 // If you've declared a variable or an object that you're not yet ready to utilize,
 // pass it to UNUSED() which will let the compiler know that you're aware of this
 // warning and would like to silence it.
@@ -12,7 +12,7 @@
 
 // Double freeing leads to segmentation faluts. This can be extremely dangerous
 // especially when the drone is in flight mode as it will very likely run out of
-// control and crash or fly into space.
+// control and crash or fly away.
 // <b>Always</b> avoid using free() directly, and use SAFE_FREE() instead. This
 // rule will be enforced and will lead to <b>pull request rejection</b> when violated.
 #define SAFE_FREE(x) \
